fe07f716ddbace118fcc6e4df9e2bf0b2e1657ed
ribas858 <ribas858@gmail.com>
2024-09-03 20:46:11 -0300

    Notificações praticamente pronto

--- meau_app/App.tsx
+++ meau_app/App.tsx
@@ -4,52 +4,62 @@ import { AutenticacaoUserProvider } from './src/assets/contexts/AutenticacaoUser
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Notifications from 'expo-notifications';
 
+// Notifications.setNotificationHandler({
+
+//     handleNotification: async (notification) => {
+
+//         let mostrarNotification: boolean = true;
+//         let mostrarNotificationChat: boolean = true;
+
+//         //console.log('HANDLENOTIFICATION ---------------------', notification);
+
+//         if (notification.request.content.data.idChat) {
+//             //console.log('idChat Notificação:', notification.request.content.data.idChat);
+//             const nomeRotaAtiva = await AsyncStorage.getItem('@rotaAtiva');
+//             if (nomeRotaAtiva) {
+//                 //console.log('nomeRotaAtiva:', nomeRotaAtiva);
+//                 const [preFixoRotaAtiva, posFixoRotaAtiva] = nomeRotaAtiva.split(':');
+//                 //console.log('Pos-fixo ROTA:', posFixoRotaAtiva);
+//                 if (posFixoRotaAtiva) {
+//                     if (posFixoRotaAtiva == notification.request.content.data.idChat) {
+//                         mostrarNotificationChat = false;
+//                         console.log('Notificação bloqueada Chat!!');
+//                     }
+//                 } else {
+//                     if (preFixoRotaAtiva) {
+//                         if (preFixoRotaAtiva == 'Conversas') {
+//                             mostrarNotification = false;
+//                             console.log('Popup bloqueado Conversas!!');
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+
+//         if (!mostrarNotificationChat) {
+//             mostrarNotification = false;
+//         }
+
+//         console.log('shouldShowAlert', (true && mostrarNotificationChat));
+//         console.log('shouldPlaySound', (mostrarNotification));
+//         console.log('shouldSetBadge', (mostrarNotification));
+
+//         return {
+//             shouldShowAlert: true && mostrarNotificationChat,
+//             shouldPlaySound: mostrarNotification,
+//             shouldSetBadge: mostrarNotification,
+//             priority: Notifications.AndroidNotificationPriority.MAX
+//         };
+//     },
+// });
+
 Notifications.setNotificationHandler({
-
-    handleNotification: async (notification) => {
-
-        let mostrarNotification: boolean = true;
-        let mostrarNotificationChat: boolean = true;
-
-        //console.log('HANDLENOTIFICATION ---------------------', notification);
-
-        if (notification.request.content.data.idChat) {
-            //console.log('idChat Notificação:', notification.request.content.data.idChat);
-            const nomeRotaAtiva = await AsyncStorage.getItem('@rotaAtiva');
-            //console.log('nomeRotaAtiva:', nomeRotaAtiva);
-            const [preFixoRotaAtiva, posFixoRotaAtiva] = nomeRotaAtiva.split(':');
-            //console.log('Pos-fixo ROTA:', posFixoRotaAtiva);
-            if (posFixoRotaAtiva) {
-                if (posFixoRotaAtiva == notification.request.content.data.idChat) {
-                    mostrarNotificationChat = false;
-                    console.log('Notificação bloqueada Chat!!');
-                }
-            } else {
-                if (preFixoRotaAtiva) {
-                    if (preFixoRotaAtiva == 'Conversas') {
-                        mostrarNotification = false;
-                        console.log('Popup bloqueado Conversas!!');
-                    }
-                }
-            }
-        }
-
-        if (!mostrarNotificationChat) {
-            mostrarNotification = false;
-        }
-
-        console.log('shouldShowAlert', (true && mostrarNotificationChat));
-        console.log('shouldPlaySound', (mostrarNotification));
-        console.log('shouldSetBadge', (mostrarNotification));
-
-        return {
-            shouldShowAlert: true && mostrarNotificationChat,
-            shouldPlaySound: mostrarNotification,
-            shouldSetBadge: mostrarNotification,
-            priority: Notifications.AndroidNotificationPriority.MAX
-        };
-    },
-});
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
 
 
 export default function App() {
