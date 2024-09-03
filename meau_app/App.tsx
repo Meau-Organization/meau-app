d2031a543696eb9159e75506185ce5cd32799d6e
ribas858 <ribas858@gmail.com>
2024-09-03 20:53:13 -0300

    ajuste

--- meau_app/App.tsx
+++ meau_app/App.tsx
@@ -53,13 +53,27 @@ import * as Notifications from 'expo-notifications';
 //     },
 // });
 
+// Notifications.setNotificationHandler({
+
+//     handleNotification: async () => {
+
+//         return {
+//             shouldShowAlert: true,
+//             shouldPlaySound: false,
+//             shouldSetBadge: false,
+//         }
+//     }
+// });
+
+
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
-      shouldShowAlert: true,
-      shouldPlaySound: false,
-      shouldSetBadge: false,
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
     }),
-  });
+});
+
 
 
 export default function App() {
